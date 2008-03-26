f743dd46897ba934eddcc832a67f018419ab24c2
baurfamily <baurfamily@dbbbb188-4547-0410-9ec2-71ac368cbdb6>
2008-03-26 06:20:35 +0000

    Tagging the released version.

---
+++ tags/v1.0b2/SmartGroup.m
@@ -0,0 +1,47 @@
+//
+//  SmartGroup.m
+//  TiVo Butler
+//
+//  Created by Eric Baur on 3/14/08.
+//  Copyright 2008 Eric Shore Baur. All rights reserved.
+//
+
+#import "SmartGroup.h"
+
+
+@implementation SmartGroup
+
+@dynamic autoDownload;
+@dynamic immediateDownload;
+@dynamic name;
+@dynamic predicate;
+@dynamic predicateString;
+
+@dynamic programs;
+
+- (NSPredicate *)predicate
+{
+	ENTRY;
+	return [NSPredicate predicateWithFormat:self.predicateString];
+}
+
+- (void)setPredicate:(NSPredicate *)newPredicate
+{
+	[self willChangeValueForKey:@"predicate"];
+	[self willChangeValueForKey:@"predicateString"];
+	self.predicateString = [newPredicate predicateFormat];
+	[self didChangeValueForKey:@"predicate"];
+	[self didChangeValueForKey:@"predicateString"];
+}
+
+- (NSSet *)programs
+{
+	ENTRY;
+	NSArray *entityArray = [EntityHelper
+		arrayOfEntityWithName:TiVoProgramEntityName
+		usingPredicate:[self predicate]
+	];
+	return [NSSet setWithArray:entityArray];
+}
+
+@end
---
+++ tags/v1.0b2/TiVoDurationValueTransformer.h
@@ -0,0 +1,16 @@
+//
+//  TiVoDurationValueTransformer.h
+//  TiVo Butler
+//
+//  Created by Eric Baur on 3/25/08.
+//  Copyright 2008 Eric Shore Baur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TiVoDurationValueTransformer : NSValueTransformer {
+
+}
+
+@end
